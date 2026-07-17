bc53c8b40095f623c890a0cdfea94b46d3dbe488
mrq-source <mr.qverse@gmail.com>
2026-07-18 02:22:20 +0530

    Co-authored-by: Atharva Sachin Bopche <atharva.bopche.041282@gmail.com>

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const hamburger = document.querySelector('.hamburger');
+const sidebar = document.querySelector('.sidebar');
+    hamburger.addEventListener('click', () => {
+        sidebar.classList.toggle('active');
+    });
+
